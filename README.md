7e5cfc12b0fc752949d33c37770ea574b4854364
fillmello <fillmello@outlook.com>
2025-03-12 18:59:52 -0300

    Versão 1.0 do jogo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Guessing-Game
-Project of a guessing game which the player chooses a random number and the program outputs if its the correct number, if is higher, or lower.
+Project of a guessing game which the player chooses a random number and the program outputs if its the correct number, if is higher, or lower. The game has different difficulties.`
--- aula.c
+++ aula.c
@@ -1,51 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
 int main(void) {
 
- 
-
-  
-
+  setlocale(LC_ALL, "Portuguese");
   srand(time(NULL));
 
 
-  int nmrscrt = (rand() % 10) + 1, nmr;
+  int nmrscrt, nmr, dificuldade, tentativas, min1, max1;
 
 
-  printf("Bem Vindo ao JOGO DA ADVINHAÇÃO\nNesse jogo você terá que adivinhar um número secreto entre 1 e 10.\nBoa Sorte!!!\n");
+  printf("Bem Vindo ao JOGO DA ADVINHAÇÃO\n");
+  printf("Escolha uma dificuldade.\n 1 - Fácil\n 2 - Médio\n 3 - Difícil\n");
+  scanf("%d", &dificuldade);
 
+  do{
+    
+    if(dificuldade == 1){
+      nmrscrt = (rand() % 10) + 1 + 1;
+      tentativas = 5;
+      min1 = 1;
+      max1 = 10;
+      printf("Nesse jogo você terá que adivinhar um número secreto entre 1 e 10.\nVocê tem 5 tentativas. Boa Sorte!!!\n");
+    }
+    else if(dificuldade == 2){
+      nmrscrt = (rand() % 50) + 1;
+      tentativas = 7;
+      max1 = 50;
+      printf("Nesse jogo você terá que adivinhar um número secreto entre 1 e 50.\nVocê tem 7 tentativas. Boa Sorte!!!\n");
+    }
 
-  for (int i = 1; i <= 3; i++){
-    printf("Tentativa %d\n", i);
-    scanf("%d", &nmr);
-
-
-  if (nmr < 0 || nmr > 10){
+    
+    else if(dificuldade == 3){
+      nmrscrt = (rand() % 100) + 1;
+      tentativas = 10;
+      printf("Nesse jogo você terá que adivinhar um número secreto entre 1 e 100.\nVocê tem 10 tentativas. Boa Sorte!!!\n");
+      max1 = 100;
+    }
+    else{
+      printf("Dificuldade Inválida\n");
   
-    printf("Número Inválido\n");}
+    }  
+    }while (dificuldade < 1 || dificuldade > 3);
 
 
 
-  else if (nmr == nmrscrt){
-  printf("Parabéns! Você acertou!\n");}
 
-  else if(nmr > nmrscrt){
-  printf("Que Pena. Você errou. O número que eu escolhi é menor que %d.\n",nmr);
-  }
 
-  else if(nmr < nmrscrt){
+  for (int i = 1; i <= tentativas; i++){
+    if (i == tentativas){
+      printf("Tentativa FINAL! \n");
+      scanf("%d", &nmr);
+      printf("O número secreto era %d\n", nmrscrt);
+    
+    
+    }else {
+      printf("Tentativa %d\n", i);
+      scanf("%d", &nmr);
+    
+
+
+
+
+
+} if (nmr < 1 || nmr > max1){
+    printf("Número Inválido\n");
+    i --;
+    
+
+
+  
+
+  
+} else if (nmr == nmrscrt){
+    printf("Parabéns! Você acertou!\n");
+    break;
+
+
+
+} else if(nmr > nmrscrt){
+    printf("Que Pena. Você errou. O número que eu escolhi é menor que %d.\n",nmr);
+
+
+} else if(nmr < nmrscrt){
     printf("OPS! Você errou. O número que eu escolhi é maior que %d.\n", nmr);
-  }
+
   
+
+
+} 
+
+
+    
   }
-  
+
+
+
+
+
+ 
+
+
+
 
   return 0;
 
-  
+
+
 }
+
+  
+
+
 
 
 
